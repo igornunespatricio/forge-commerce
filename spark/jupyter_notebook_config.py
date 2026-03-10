a74ae185569cc93e57b1e13d97615728177e0051
Igor Nunes Patricio <igornunespatricio@gmail.com>
2026-03-09 23:16:05 -0300

    add spark submit and jupyter containers

---
+++ spark/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+# jupyter_notebook_config.py
+c.ServerApp.token = ''
+c.ServerApp.password = ''
+c.ServerApp.disable_check_xsrf = True
+c.ServerApp.allow_origin = '*'
+c.ServerApp.allow_remote_access = True
+c.ServerApp.open_browser = False
+c.ServerApp.ip = '0.0.0.0'
